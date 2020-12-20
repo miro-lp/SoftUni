cbb736b6f1c91f2d9e9bfbedaeafbb6c810f44b7
miro <miro_lp@abv.bg>
2020-12-20 17:06:08 +0200

    Upload

--- PythonAdvanced/TUPLESandSETS/BattleofNames.py
+++ PythonAdvanced/TUPLESandSETS/BattleofNames.py
@@ -17,7 +17,7 @@ for i in range(n):
         set_odd.add(value)
 if sum(set_odd) == sum(set_even):
     print(", ".join([str(i) for i in (set_even | set_odd)]))
-elif sum(set_odd)>sum(set_even):
+elif sum(set_odd) > sum(set_even):
     print(", ".join([str(i) for i in (set_odd - set_even)]))
 else:
     print(", ".join([str(i) for i in (set_odd ^ set_even)]))

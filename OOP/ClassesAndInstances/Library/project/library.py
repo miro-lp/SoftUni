e5c270cc05946eb4fc863ccbaa899bbe72626dc4
miro <miro_lp@abv.bg>
2021-03-01 18:27:54 +0200

    Upload

--- OOP/ClassesAndInstances/Library/project/library.py
+++ OOP/ClassesAndInstances/Library/project/library.py
@@ -1,17 +1,15 @@
-from user import User
-
 class Library:
     def __init__(self):
         self.user_records = []
         self.books_available = {}
         self.rented_books = {}
 
-    def add_user(self, user: User):
+    def add_user(self, user):
         if user.user_id in [u.user_id for u in self.user_records]:
-            return f"User with id = {user.user_id} already registered in the library"
+            return f"User with id = {user.user_id} already registered in the library!"
         self.user_records.append(user)
 
-    def remove_user(self, user: User):
+    def remove_user(self, user):
         if user not in self.user_records:
             return f"We could not find such user to remove!"
         self.user_records.remove(user)
@@ -35,31 +33,4 @@ class Library:
 
 
 
-user = User(12, 'Peter')
-library = Library()
-library.add_user(user)
-print(library.add_user(user))
-library.remove_user(user)
-print(library.remove_user(user))
-library.add_user(user)
-print(library.change_username(2, 'Igor'))
-print(library.change_username(12, 'Peter'))
-print(library.change_username(12, 'George'))
-[print(f'{user_record.user_id}, {user_record.username}, {user_record.books}') for user_record in library.user_records]
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
 
-user.get_book('J.K.Rowling', 'The Deathly Hallows', 17, library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
-print(user.get_book('J.K.Rowling', 'The Deathly Hallows', 10, library))
-print(user.return_book('J.K.Rowling', 'The Cursed Child', library))
-user.return_book('J.K.Rowling', 'The Deathly Hallows', library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)

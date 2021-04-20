63c92c9f2086ddca8f0e340abf9ac952f210003e
miro <miro_lp@abv.bg>
2021-04-20 09:22:47 +0300

    Upload

---
+++ petstagram/petstagram/pets/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+
+from petstagram.pets.models import Pet, Like
+
+
+def list_pets(request):
+    all_pets = Pet.objects.all()
+    context = {'pets': all_pets}
+    return render(request,'pets/pet_list.html',context)
+
+
+def pet_details(request, pk):
+    pet = Pet.objects.get(pk=pk)
+    pet.likes_count = pet.like_set.count()
+    context = {'pet':pet}
+    return render(request,'pets/pet_detail.html',context)
+
+
+def like_pet(request, pk):
+    pet_to_like = Pet.objects.get(pk=pk)
+    like = Like(pet = pet_to_like)
+    like.save()
+    return redirect('pet details', pet_to_like.id)

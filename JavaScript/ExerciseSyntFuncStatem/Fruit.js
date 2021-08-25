283d9328537919de49f7f6a301e58593bae9ca2a
Miro <75385008+miro-lp@users.noreply.github.com>
2021-08-25 18:10:18 +0300

    Delete JavaScript/ExerciseSyntFuncStatem directory

--- JavaScript/ExerciseSyntFuncStatem/Fruit.js
+++
@@ -1,7 +0,0 @@
-function fruit(fruit, weight, cost){
-    let price = (weight*cost)/1000;
-    console.log(`I need \$${price.toFixed(2)} to buy ${(weight/1000).toFixed(2)} kilograms ${fruit}.`)
-}
-
-fruit('orange', 2500, 1.80)
-fruit('apple', 1563, 2.35)
